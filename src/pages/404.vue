ebda2cc0abb1c0f10cd461fe1e94cfada51c378d
Analia Mok <analiam319@gmail.com>
2019-04-09 20:48:25 -0400

    Created 404 page. Updated gitignore.

---
+++ src/pages/404.vue
@@ -0,0 +1,18 @@
+<template functional>
+  <Layout class="text-center min-h-screen">
+    <div class="py-24 sm:py-16 lg:py-20 lg:pb-32">
+      <h1 class="font-display text-5xl error-title text-blue-darker">404</h1>
+      <p class="font-sans text-black">Sorry, that page does not exist.</p>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.error-title {
+  font-size: 7rem;
+}
+</style>
